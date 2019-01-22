eb53ffaffacca27a145e29a97c2f57dc5255c695
Thibaut Fatus <tfatus@gmail.com>
2019-01-22 09:40:07 +0100

    chore: yapf

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
 
-setup(name='serenity',
-      version='0.1',
-      description='Serenity API',
-      url='http://github.com/weassur/serenity',
-      author='Thibaut Fatus',
-      author_email='thibaut@weassur.com',
-      license='GPL 3.0',
-      packages=['serenity'],
-      zip_safe=False)
+setup(
+    name='serenity',
+    version='0.1',
+    description='Serenity API',
+    url='http://github.com/weassur/serenity',
+    author='Thibaut Fatus',
+    author_email='thibaut@weassur.com',
+    license='GPL 3.0',
+    packages=['serenity'],
+    install_requires=[
+        'PyJWT',
+    ],
+    zip_safe=False)
